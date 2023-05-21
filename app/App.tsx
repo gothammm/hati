aec010318b478cccebd5ba19fa06c46488450d14
Gautham Ramachandran <gautham2r@gmail.com>
2023-05-21 21:46:38 +0200

    feat:setup mdx

--- app/App.tsx
+++ app/App.tsx
@@ -1,51 +1,14 @@
-<<<<<<< HEAD
-import { Component, Match, Switch, createSignal } from "solid-js";
-import {
-  Button,
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardSubtitle,
-  CardTitle,
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-  Alert,
-  AlertDescription,
-  AlertTitle,
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  Badge,
-} from "@gothammm/hati";
-import {
-  AlertCircle,
-  AlertTriangle,
-  Loader2,
-  Mail,
-  Moon,
-  Save,
-  Sun,
-  Terminal,
-} from "lucide-solid";
-import { PreviewContainer } from "./components/PreviewContainer";
-import { applyTheme } from "../lib/util";
-import { TabsPreview } from "./components/TabsPreview";
-import { DialogPreview } from "./components/DialogPreview";
-=======
 import { Routes, Route } from "@solidjs/router";
 import { Header } from "./components/header";
 import { Home } from "./pages/home";
->>>>>>> replace new site with old
+import Hello from "./pages/hello.mdx";
 
 export default function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/button" component={() => "Hello"} />
+        <Route path="/button" component={Hello} />
         <Route path="/" component={Home} />
         <Route
           path="/about"
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), {
+    enforce: "pre",
+    ...mdx({
+      jsxImportSource: "solid-js/h",
+    }),
+  }],
   server: {
     port: 3000,
   },
