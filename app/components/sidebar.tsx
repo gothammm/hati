95fedbe69534593c5d34e78be43d7d0c3fe33f93
Gautham Ramachandran <gautham2r@gmail.com>
2023-05-23 22:08:44 +0200

    Add styles to mdx, add sidebar

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,31 @@
+import "./sidebar.css";
+
+export const Sidebar = () => {
+  return (
+    <div class="hidden lg:relative lg:block lg:flex-none">
+      <div class="sticky top-[61px] h-[calc(100vh-61px)] overflow-y-auto py-6 pe-2 ps-2">
+        <nav class="w-52 text-base lg:text-sm">
+          <ul class="space-y-6">
+            <li>
+              <h2 class="font-display ms-3 font-medium text-accent-foreground">
+                Overview
+              </h2>
+              <ul class="mt-2 space-y-1 text-sm">
+                <li class="group relative">
+                  <a href="#" link>
+                    Introduction
+                  </a>
+                </li>
+                <li class="group relative">
+                  <a href="#" link>
+                    Getting Started
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
